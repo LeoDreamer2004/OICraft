45ad5bed117c3568774b2cfb43e1a140bfa89dfe
LeoDreamer <110815661+LeoDreamer2004@users.noreply.github.com>
2024-04-16 19:06:54 +0800

    Initialize

---
+++ src/main/java/org/dindier/oicraft/OiCraftApplication.java
@@ -0,0 +1,13 @@
+package org.dindier.oicraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OiCraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OiCraftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/dindier/oicraft/OiCraftApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dindier.oicraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OiCraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
