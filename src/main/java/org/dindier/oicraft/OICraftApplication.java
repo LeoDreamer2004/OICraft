7b2ab3abd6c83a21cdd41f727841aa8e055e44c8
LeoDreamer <110815661+LeoDreamer2004@users.noreply.github.com>
2024-04-18 14:34:37 +0800

    Update OICraftApplication.java

--- src/main/java/org/dindier/oicraft/OICraftApplication.java
+++ src/main/java/org/dindier/oicraft/OICraftApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OiCraftApplication {
+public class OICraftApplication {
     public static void main(String[] args) {
-        SpringApplication.run(OiCraftApplication.class, args);
+        SpringApplication.run(OICraftApplication.class, args);
     }
 }
