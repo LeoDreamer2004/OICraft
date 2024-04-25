0e16bb9c623fba1c12b6095e4e33803c4dcc0447
LeoDreamer <110815661+LeoDreamer2004@users.noreply.github.com>
2024-04-25 14:39:46 +0800

    A new function for check in

--- src/main/java/org/dindier/oicraft/service/UserService.java
+++ src/main/java/org/dindier/oicraft/service/UserService.java
@@ -5,6 +5,14 @@ import org.dindier.oicraft.model.User;
 import org.springframework.lang.Nullable;
 
 public interface UserService {
+
+    /**
+     * Create a new user, with password encoded
+     * @param user The user to create
+     * @return The created user object
+     */
+    User createUser(User user);
+
     /**
      * Get user by request
      *
@@ -14,5 +22,18 @@ public interface UserService {
     @Nullable
     User getUserByRequest(HttpServletRequest request);
 
-    User createUser(User user);
+    /**
+     * Record the check-in of a user & add EXP etc.
+     *
+     * @param user The user to check in
+     * @implNote Check-in is a daily task, so the user can only check in once a day
+     */
+    void checkIn(User user);
+
+    /**
+     * Check if the user has checked in today
+     * @param user The user to check
+     * @return Whether the user has checked in today
+     */
+    boolean hasCheckedInToday(User user);
 }
