6ee91a0f8b9507e43a28d2b0340d3ef87bac829e
LeoDreamer <110815661+LeoDreamer2004@users.noreply.github.com>
2024-04-26 15:59:46 +0800

    Admin pages

--- src/main/java/org/dindier/oicraft/controller/AdminController.java
+++ src/main/java/org/dindier/oicraft/controller/AdminController.java
@@ -5,11 +5,12 @@ import org.dindier.oicraft.dao.UserDao;
 import org.dindier.oicraft.model.User;
 import org.dindier.oicraft.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class AdminController {
@@ -20,21 +21,88 @@ public class AdminController {
 
     @GetMapping("/admin")
     public ModelAndView admin() {
-        return new ModelAndView("user/admin")
+        return new ModelAndView("admin/admin")
                 .addObject("users", userDao.getAllUsers());
     }
 
-    @GetMapping("admin/delete/{id}")
-    public ResponseEntity<String> deleteUser(@PathVariable int id) {
+    @GetMapping("/admin/delete/{id}")
+    public ModelAndView deleteUser(@PathVariable int id) {
         User user = userDao.getUserById(id);
+        String error = canEditUser(user);
+        if (error != null)
+            return new ModelAndView("admin/error")
+                    .addObject("error", error);
+        return new ModelAndView("admin/delete")
+                .addObject("deleteUser", user);
+    }
+
+    @PostMapping("/admin/delete/{id}")
+    public ModelAndView deleteUserConfirm(@PathVariable int id) {
+        User user = userDao.getUserById(id);
+        String error = canEditUser(user);
+        if (error != null)
+            return new ModelAndView("admin/error")
+                    .addObject("error", error);
+        userDao.deleteUser(user);
+        return new ModelAndView("admin/admin");
+    }
+
+    @GetMapping("admin/upgrade/{id}")
+    public ModelAndView upgradeUser(@PathVariable int id) {
+        User user = userDao.getUserById(id);
+        String error = canEditUser(user);
+        if (error != null)
+            return new ModelAndView("admin/error")
+                    .addObject("error", error);
+        return new ModelAndView("admin/upgrade")
+                .addObject("upgradeUser", user);
+    }
+
+    @PostMapping("admin/upgrade/{id}")
+    public Object upgradeUserConfirm(@PathVariable int id) {
+        User user = userDao.getUserById(id);
+        String error = canEditUser(user);
+        if (error != null)
+            return new ModelAndView("admin/error")
+                    .addObject("error", error);
+        user.setRole(User.Role.ADMIN);
+        userDao.updateUser(user);
+        return new RedirectView("/admin");
+    }
+
+    @GetMapping("admin/downgrade/{id}")
+    public ModelAndView downgradeUser(@PathVariable int id) {
+        User user = userDao.getUserById(id);
+        String error = canEditUser(user);
+        if (error != null)
+            return new ModelAndView("admin/error")
+                    .addObject("error", error);
+        return new ModelAndView("admin/downgrade")
+                .addObject("downgradeUser", user);
+    }
+
+    @PostMapping("admin/downgrade/{id}")
+    public Object downgradeUserConfirm(@PathVariable int id) {
+        User user = userDao.getUserById(id);
+        String error = canEditUser(user);
+        if (error != null)
+            return new ModelAndView("admin/error")
+                    .addObject("error", error);
+        user.setRole(User.Role.USER);
+        userDao.updateUser(user);
+        return new RedirectView("/admin");
+    }
+
+    /* Check if the user can be edited */
+    private String canEditUser(User user) {
         User currentUser = userService.getUserByRequest(request);
-        if (currentUser == null)
-            return null;
         if (user == null)
-            return ResponseEntity.badRequest().body("此用户不存在！");
+            return "用户不存在";
+        if (currentUser == null)
+            return "请先登录";
         if (currentUser.equals(user))
-            return ResponseEntity.badRequest().body("不能删除自己！");
-        return ResponseEntity.ok("确认删除用户 " + user.getName() + " 吗？");
+            return "不能修改自己的账户";
+        return null;
     }
 
     @Autowired
@@ -51,5 +119,4 @@ public class AdminController {
     public void setRequest(HttpServletRequest request) {
         this.request = request;
     }
-
 }
