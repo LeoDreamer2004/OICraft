6bcd837ed84e4e09505331c5b9196933186b2fa0
20484 <2200010626@stu.pku.edu.cn>
2024-04-26 22:53:16 +0800

    implement of email

--- src/main/java/org/dindier/oicraft/service/impl/UserServiceImpl.java
+++ src/main/java/org/dindier/oicraft/service/impl/UserServiceImpl.java
@@ -1,15 +1,21 @@
 package org.dindier.oicraft.service.impl;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.antlr.v4.runtime.misc.Pair;
 import org.dindier.oicraft.model.User;
 import org.dindier.oicraft.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.dindier.oicraft.dao.UserDao;
 
 import java.sql.Date;
 import java.util.Calendar;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.Logger;
 
 @Service("userService")
@@ -17,10 +23,17 @@ public class UserServiceImpl implements UserService {
     private UserDao userDao;
     private PasswordEncoder passwordEncoder;
     private final Logger logger = Logger.getLogger(UserServiceImpl.class.getName());
+    private JavaMailSender mailSender;
+    private Map<Pair<String,String>, String> verificationCodes = new ConcurrentHashMap<>();
 
     @Autowired
     private void setUserDao(UserDao userDao) {
         this.userDao = userDao;
+    }
+
+    @Autowired
+    private void setMailSender(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
     }
 
     @Autowired
@@ -78,6 +91,16 @@ public class UserServiceImpl implements UserService {
     @Override
     public void sendVerificationCode(HttpServletRequest request, String email) {
         // TODO: Implement this method
+        String username = request.getRemoteUser();
+        String verificationCode = UUID.randomUUID().toString();
+        Pair<String,String> key = new Pair<>(username, email);
+        verificationCodes.put(key, verificationCode);
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setFrom("oicraft2024@163.com");
+        mailMessage.setTo(email);
+        mailMessage.setSubject("OICraft: Your verification code");
+        mailMessage.setText("Your verification code is " + verificationCode);
+        mailSender.send(mailMessage);
         logger.info("Verification code sent to " + email);
     }
 
@@ -87,6 +110,14 @@ public class UserServiceImpl implements UserService {
         User user = getUserByRequest(request);
         if (user == null)
             return false;
+        String username = user.getUsername();
+        Pair<String,String> key = new Pair<>(username, email);
+        String correctCode = verificationCodes.get(key);
+        if (correctCode == null || !correctCode.equals(code))
+            return false;
+        verificationCodes.remove(key);
+        user.setEmail(email);
+        userDao.updateUser(user);
         logger.info("Binding email " + email + " for user " + user.getUsername());
         return true;
     }
