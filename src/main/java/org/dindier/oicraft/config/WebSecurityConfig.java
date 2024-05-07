b962eef54664bddfd5500c5a59d9bfb73fce4bbe
LeoDreamer <110815661+LeoDreamer2004@users.noreply.github.com>
2024-05-07 17:38:46 +0800

    update report

--- src/main/java/org/dindier/oicraft/config/WebSecurityConfig.java
+++ src/main/java/org/dindier/oicraft/config/WebSecurityConfig.java
@@ -14,7 +14,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity()
+@EnableMethodSecurity
 public class WebSecurityConfig {
     private final AuthenticationConfiguration authenticationConfiguration;
 
