2a0492f73735b71b684e1397e1d8b3cc2b8c6076
crlcrl1 <crl18039102576@126.com>
2024-05-14 20:47:20 +0800

    Add AI utils

--- src/main/java/org/dindier/oicraft/config/SpringConfig.java
+++ src/main/java/org/dindier/oicraft/config/SpringConfig.java
@@ -2,8 +2,10 @@ package org.dindier.oicraft.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan(basePackages = "org.dindier.oicraft")
+@PropertySource("classpath:/ai.properties")
 public class SpringConfig {
 }
