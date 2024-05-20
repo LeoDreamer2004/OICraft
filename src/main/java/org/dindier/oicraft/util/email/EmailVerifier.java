d881820ade1c5f5a00b4b42163f7307fe32220de
LeoDreamer <110815661+LeoDreamer2004@users.noreply.github.com>
2024-05-20 23:47:24 +0800

    Optimize code for email verify & search

---
+++ src/main/java/org/dindier/oicraft/util/email/EmailVerifier.java
@@ -0,0 +1,101 @@
+package org.dindier.oicraft.util.email;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.antlr.v4.runtime.misc.Pair;
+import org.dindier.oicraft.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Email verification utility class
+ */
+@Slf4j
+@Component
+public class EmailVerifier {
+    private final Map<Pair<String, String>, VerificationCode> verificationCodes
+            = new ConcurrentHashMap<>();
+    private final long VALID_TIME = TimeUnit.MINUTES.toMillis(5);
+    private JavaMailSender mailSender;
+
+    @Autowired
+    public void setMailSender(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    @Getter
+    public static class VerificationCode {
+        private final String code;
+        private final long timestamp;
+
+        public VerificationCode(String code) {
+            this.code = code;
+            this.timestamp = System.currentTimeMillis();
+        }
+    }
+
+    public void send(User user, String email) {
+        String username = user.getUsername();
+        String verificationCode = UUID.randomUUID().toString();
+        Pair<String, String> key = new Pair<>(username, email);
+        verificationCodes.put(key, new VerificationCode(verificationCode));
+        MimeMessage mailMessage = mailSender.createMimeMessage();
+        // read email.html and replace the username and placeholder with the verification code
+        URL emailUrl = getClass().getClassLoader()
+                .getResource("static/html/email.html");
+        if (emailUrl == null) {
+            log.warn("Error while trying to send email to {}", email);
+            return;
+        }
+        String htmlMsg;
+        try (InputStream emailStream = emailUrl.openStream()) {
+            htmlMsg = new String(emailStream.readAllBytes());
+        } catch (IOException e) {
+            log.warn("Error while trying to send email to {}", email);
+            return;
+        }
+        htmlMsg = htmlMsg.replace("{{username}}", username);
+        htmlMsg = htmlMsg.replace("{{code}}", verificationCode);
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(mailMessage, false, "utf-8");
+            mailMessage.setContent(htmlMsg, "text/html");
+            helper.setTo(email);
+            helper.setSubject("Your OICraft verification code");
+            helper.setFrom("oicraft2024@163.com");
+        } catch (MessagingException e) {
+            log.info("Error while trying to send email to {}", email);
+        }
+        mailSender.send(mailMessage);
+        log.info("Verification code sent to {}", email);
+    }
+
+    // FIXME: Auto-delete the verification code in 5 minutes
+    public boolean verify(User user, String email, String code) {
+        String username = user.getUsername();
+        Pair<String, String> key = new Pair<>(username, email);
+        if (!verificationCodes.containsKey(key))
+            return false;
+        String correctCode = verificationCodes.get(key).getCode();
+        if (correctCode == null || !correctCode.equals(code))
+            return false;
+        if (System.currentTimeMillis() - verificationCodes.get(key).getTimestamp() > VALID_TIME) {
+            verificationCodes.remove(key);
+            return false;
+        }
+        verificationCodes.remove(key);
+        log.info("Email from {} verified successfully", email);
+        return true;
+    }
+}
---
+++ src/main/java/org/dindier/oicraft/util/search/SearchHelper.java
@@ -0,0 +1,115 @@
+package org.dindier.oicraft.util.search;
+
+import lombok.Setter;
+import org.apache.lucene.analysis.cn.smart.SmartChineseAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.ByteBuffersDirectory;
+import org.apache.lucene.store.Directory;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.util.*;
+import java.util.function.Function;
+
+public class SearchHelper<T> {
+
+    @Setter
+    private List<T> items; // the items to search
+    @Setter
+    private int maxSearchResult = 50; // the maximum number of search results
+    private String identifier; // the field that uniquely identifies the item
+
+    private final Map<String, Function<T, String>> fieldsMap = new HashMap<>();
+    private final Map<String, Float> boosts = new HashMap<>();
+
+    /**
+     * Set the identifier of the items
+     *
+     * @param identifier the name of the identifier field
+     * @param getter     a function that gets the identifier value from the item
+     */
+    public void setIdentifier(String identifier, Function<T, String> getter) {
+        this.identifier = identifier;
+        fieldsMap.put(identifier, getter);
+    }
+
+    /**
+     * Add a field to the search helper
+     *
+     * @param field  the name of the field
+     * @param getter a function that gets the field value from the item
+     * @param boost  the boost of the field
+     */
+    public void addField(String field, Function<T, String> getter, float boost) {
+        fieldsMap.put(field, getter);
+        boosts.put(field, boost);
+    }
+
+    /**
+     * Search the items with the keyword
+     *
+     * @param keyword the keyword to search
+     * @return a list of identifiers of the items that match the keyword
+     */
+    public List<T> search(String keyword) {
+        List<String> identifiers = new ArrayList<>();
+        try (Directory directory = new ByteBuffersDirectory();
+             IndexWriter indexWriter = new IndexWriter(directory,
+                     new IndexWriterConfig(new SmartChineseAnalyzer()))
+        ) {
+            // index the items
+            for (T t : items) {
+                Document document = new Document();
+                for (Map.Entry<String, Function<T, String>> entry : fieldsMap.entrySet()) {
+                    document.add(new TextField(entry.getKey(), entry.getValue().apply(t), Field.Store.YES));
+                }
+                indexWriter.addDocument(document);
+            }
+            indexWriter.commit();
+            indexWriter.close();
+
+            // parse the query
+            String[] fields = new String[fieldsMap.size() - 1]; // should not include the identifier
+            int cnt = 0;
+            for (String field : fieldsMap.keySet()) {
+                if (!field.equals(identifier)) {
+                    fields[cnt++] = field;
+                }
+            }
+
+            MultiFieldQueryParser parser = new MultiFieldQueryParser(
+                    fields, new SmartChineseAnalyzer(), boosts);
+            Query query;
+            try {
+                query = parser.parse(keyword);
+            } catch (ParseException e) {
+                throw new RuntimeException(e);
+            }
+
+            // search the problems
+            try (IndexReader indexReader = DirectoryReader.open(directory)) {
+                IndexSearcher indexSearcher = new IndexSearcher(indexReader);
+                TopDocs topDocs = indexSearcher.search(query, maxSearchResult);
+                for (ScoreDoc scoreDoc : topDocs.scoreDocs) {
+                    identifiers.add(indexSearcher.doc(scoreDoc.doc).get(identifier));
+                }
+            }
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+        Function<T, String> identifierGetter = fieldsMap.get(identifier);
+        return items.stream().filter(t -> identifiers.contains(identifierGetter.apply(t))).toList();
+    }
+}
