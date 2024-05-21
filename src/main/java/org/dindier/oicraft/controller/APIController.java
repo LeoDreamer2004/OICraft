74023d3fc1310b887669df6641d85fa7b46f6809
leodreamer <913116367@qq.com>
2024-05-21 18:53:59 +0800

    personal link & submission query for user

---
+++ src/main/java/org/dindier/oicraft/controller/APIController.java
@@ -0,0 +1,29 @@
+package org.dindier.oicraft.controller;
+
+import org.dindier.oicraft.model.User;
+import org.dindier.oicraft.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class APIController {
+
+    UserService userService;
+
+    @GetMapping("/api/user")
+    public ResponseEntity<Integer> getUserId(@RequestParam String username) {
+        User user = userService.getUserByUsername(username);
+        if (user == null)
+            return ResponseEntity.badRequest().body(-1);
+        System.out.println(user.getId() + user.getUsername());
+        return ResponseEntity.ok(user.getId());
+    }
+
+    @Autowired
+    private void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+}
