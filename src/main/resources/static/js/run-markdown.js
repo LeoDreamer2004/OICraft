4ea0e440ce41a757292299d4b4ff669b7f505e99
LeoDreamer <110815661+LeoDreamer2004@users.noreply.github.com>
2024-05-22 16:38:41 +0800

    Fix bug on rendering code

--- src/main/resources/static/js/run-markdown.js
+++ src/main/resources/static/js/run-markdown.js
@@ -1,4 +1,16 @@
 let markdowns = document.getElementsByClassName('markdown');
 for (i = 0; i < markdowns.length; i++) {
-    markdowns[i].innerHTML = marked.parse(markdowns[i].innerHTML);
+    let temp = marked.parse(markdowns[i].innerHTML);
+    // There may some bugs in <pre><code>
+    // So replace &amp; to &
+    let reg = /<pre><code.*?>[\s\S]*<\/code><\/pre>/g;
+    let match = temp.match(reg);
+    if (match) {
+        for (j = 0; j < match.length; j++) {
+            let code = match[j];
+            code = code.replace(/&amp;/g, '&');
+            temp = temp.replace(match[j], code);
+        }
+    }
+    markdowns[i].innerHTML = temp;
 }
